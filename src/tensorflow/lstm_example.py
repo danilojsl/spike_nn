5dd8c8eb1235fb6c289a06078c206b78ab1e4018
danilojsl <danilo@johnsnowlabs.com>
2021-02-02 08:07:59 +0100

    Refactor lstm example for tensorflow

--- src/tensorflow/lstm_example.py
+++ src/tensorflow/lstm_example.py
@@ -4,36 +4,37 @@ from tensorflow.keras.layers import LSTM
 import numpy as np
 
 
-def simple_lstm():
-    """
-    Define Model:
-    Very small model with a single LSTM layer that itself contains a LSTM cell of size 2 (hidden state of length 2).
-    In this example, we will have one input sample with 3 time steps and one feature observed at each time step
-    Check book page 52 for parameters
-    """
-    time_steps = 3
-    hidden_state_size = 2
-    number_features = 2
+class SimpleLstm:
 
-    inputs1 = Input(shape=(time_steps, number_features))
-    lstm1, hidden_states, cell_state = LSTM(hidden_state_size, return_sequences=True, return_state=True)(inputs1)
-    model = Model(inputs=inputs1, outputs=[lstm1, hidden_states, cell_state])
+    def __init__(self):
+        # LSTM Network architecture
+        self.time_steps = 3
+        self.number_features = 1
+        self.hidden_state_size = 2
+        # Input data
+        self.sample_size = 1  # batch size
 
-    # define input data
-    sample_size = 1  # batch size
-    random_input = np.random.rand(sample_size, time_steps)
-    input_sequence = random_input.reshape((sample_size, time_steps, number_features))
+    def forward(self):
+        inputs1 = Input(shape=(self.time_steps, self.number_features))
+        lstm1, hidden_states, cell_state = \
+            LSTM(self.hidden_state_size, return_sequences=True, return_state=True)(inputs1)
+        return Model(inputs=inputs1, outputs=[lstm1, hidden_states, cell_state])
 
-    # make and show prediction
-    model.summary()
-    output = model.predict(input_sequence)
-    hidden_states = output[0]
-    hidden_state = output[1]
-    cell_state = output[2]
-    print("Hidden State for each input time step: " + str(hidden_states))
-    print("Hidden State for last time step: " + str(hidden_state))
-    print("Cell State for last time step: " + str(cell_state))
+    def prediction(self, model):
+        # make and show prediction
+        model.summary()
+        random_input = np.random.rand(self.sample_size, self.time_steps)
+        input_sequence = random_input.reshape((self.sample_size, self.time_steps, self.number_features))
+        output = model.predict(input_sequence)
+        hidden_states = output[0]
+        hidden_state = output[1]
+        cell_state = output[2]
+        print("Hidden State for each input time step: " + str(hidden_states))
+        print("Hidden State for last time step: " + str(hidden_state))
+        print("Cell State for last time step: " + str(cell_state))
 
 
 if __name__ == '__main__':
-    simple_lstm()
+    lstm_net = SimpleLstm()
+    lstm_model = lstm_net.forward()
+    lstm_net.prediction(lstm_model)
