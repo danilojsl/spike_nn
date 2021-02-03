d8cf0725216ad2827dfd6caca8588189dda35cfb
danilojsl <danilo@johnsnowlabs.com>
2021-02-03 06:26:58 +0100

    Refactor lstm example for pytorch

--- src/pytorch/lstm_example.py
+++ src/pytorch/lstm_example.py
@@ -8,28 +8,37 @@ class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
+        # LSTM Network architecture
         self.number_features = 1
-        hidden_state_size = 2
-        self.sample_size = 1
-        num_layers = 1
+        self.hidden_state_size = 2
+        self.num_layers = 1
 
-        self.initial_hidden_state = torch.zeros(num_layers, self.sample_size, hidden_state_size)  # h_0
-        self.initial_cell_state = torch.zeros(num_layers, self.sample_size, hidden_state_size)  # c_0
-        self.lstm1 = nn.LSTM(input_size=self.number_features, hidden_size=hidden_state_size)
-        print(self.lstm1)
+        # Input data
+        self.sample_size = 1
+        self.time_steps = 3  # sequence length
 
     def forward(self):
-        time_steps = 3  # sequence length
-        random_input = torch.rand((self.sample_size, time_steps))
-        input_sequence = random_input.reshape(time_steps, self.sample_size, self.number_features)
+        return nn.LSTM(input_size=self.number_features, hidden_size=self.hidden_state_size)
 
-        output_features, (hidden_state, cell_state) = self.lstm1(input_sequence, (self.initial_hidden_state,
-                                                                                  self.initial_cell_state))
+    def prediction(self, lstm_model):
+        input_sequence = self.get_input()
+
+        initial_hidden_state = torch.zeros(self.num_layers, self.sample_size, self.hidden_state_size)  # h_0
+        initial_cell_state = torch.zeros(self.num_layers, self.sample_size, self.hidden_state_size)  # c_0
+        output_features, (hidden_state, cell_state) = lstm_model(input_sequence,
+                                                                 (initial_hidden_state, initial_cell_state))
+
         print("Output Features (h_t) from last layer of LSTM: " + str(output_features))
         print("Hidden State for t=seq_len : " + str(hidden_state))
         print("Cell State for t=seq_len : " + str(cell_state))
 
+    def get_input(self):
+        random_input = torch.rand((self.sample_size, self.time_steps))
+        input_sequence = random_input.reshape(self.time_steps, self.sample_size, self.number_features)
+        return input_sequence
+
 
 if __name__ == '__main__':
     lstm_net = Net()
-    lstm_net.forward()
+    simple_lstm_model = lstm_net.forward()
+    lstm_net.prediction(simple_lstm_model)
--- src/tensorflow/lstm_example.py
+++ src/tensorflow/lstm_example.py
@@ -11,6 +11,7 @@ class SimpleLstm:
         self.time_steps = 3
         self.number_features = 1
         self.hidden_state_size = 2
+
         # Input data
         self.sample_size = 1  # batch size
 
@@ -20,12 +21,11 @@ class SimpleLstm:
             LSTM(self.hidden_state_size, return_sequences=True, return_state=True)(inputs1)
         return Model(inputs=inputs1, outputs=[lstm1, hidden_states, cell_state])
 
-    def prediction(self, model):
-        # make and show prediction
-        model.summary()
-        random_input = np.random.rand(self.sample_size, self.time_steps)
-        input_sequence = random_input.reshape((self.sample_size, self.time_steps, self.number_features))
-        output = model.predict(input_sequence)
+    def prediction(self, lstm_model):
+        lstm_model.summary()
+        input_sequence = self.get_input()
+        output = lstm_model.predict(input_sequence)
+
         hidden_states = output[0]
         hidden_state = output[1]
         cell_state = output[2]
@@ -33,8 +33,13 @@ class SimpleLstm:
         print("Hidden State for last time step: " + str(hidden_state))
         print("Cell State for last time step: " + str(cell_state))
 
+    def get_input(self):
+        random_input = np.random.rand(self.sample_size, self.time_steps)
+        input_sequence = random_input.reshape((self.sample_size, self.time_steps, self.number_features))
+        return input_sequence
+
 
 if __name__ == '__main__':
     lstm_net = SimpleLstm()
-    lstm_model = lstm_net.forward()
-    lstm_net.prediction(lstm_model)
+    simple_lstm_model = lstm_net.forward()
+    lstm_net.prediction(simple_lstm_model)
